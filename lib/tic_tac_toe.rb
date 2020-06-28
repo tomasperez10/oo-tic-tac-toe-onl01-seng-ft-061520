913a6d36dfd4132af87f347065b58adb2f357f97
Tomas Perez <tomasitopg6@gmail.com>
2020-06-28 00:25:25 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -57,11 +57,11 @@ class TicTacToe
 end
 
 def won?
-  WIN_COMBINATIONS.detect do |win_combo|
-    if (@board[win_combo[0]]) == "X" && (@board[win_combo[1]]) == "X" && (@board[win_combo[2]]) == "X"
-      return win_combo
-    elsif (@board[win_combo[0]]) == "O" && (@board[win_combo[1]]) == "O" && (@board[win_combo[2]]) == "O"
-      return win_combo
+  WIN_COMBINATIONS.detect do |combinations|
+    if (@board[combinations[0]]) == "X" && (@board[combinations[1]]) == "X" && (@board[combinations[2]]) == "X"
+      return combinations
+    elsif (@board[combinations[0]]) == "O" && (@board[combinations[1]]) == "O" && (@board[combinations[2]]) == "O"
+      return combinations
     end
       false
   end
