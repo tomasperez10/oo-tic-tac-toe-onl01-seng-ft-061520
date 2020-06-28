00dd06a0f77482f0b483333063bd2327a440d4fa
Tomas Perez <tomasitopg6@gmail.com>
2020-06-28 00:29:32 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -92,7 +92,18 @@ end
     end
   end
   
-  
+  def play
+    while over? == false
+      turn
+    end
+    if won?
+      puts "Congratulations #{winner}!"
+    elsif draw?
+      puts "Cat's Game!"
+      end
+    end
+
+  end
   
  
   def turn_count
