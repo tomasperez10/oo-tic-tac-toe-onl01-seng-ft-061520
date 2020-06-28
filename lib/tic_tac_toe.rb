03e8e9ba76239b4f711bb51b84971e5c1551dd23
Tomas Perez <tomasitopg6@gmail.com>
2020-06-28 00:23:34 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -29,6 +29,15 @@ class TicTacToe
   def valid_move?(index)
     index.between?(0,8) && !position_taken?(index)
   end
+  
+  def turn_count
+    number_of_turns = 0
+    @board.each do |space|
+    if space == "X" || space == "O"
+        number_of_turns += 1
+    end
+  end
+  return number_of_turns
  
   def current_player
     turn_count % 2 == 0 ? "X" : "O"
