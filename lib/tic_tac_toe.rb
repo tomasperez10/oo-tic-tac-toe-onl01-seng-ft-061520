ab8116e0e4752b51ebff37621fd0f832a3ee17e9
Tomas Perez <tomasitopg6@gmail.com>
2020-06-28 00:24:34 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -55,6 +55,17 @@ class TicTacToe
       turn
     end
 end
+
+def won?
+  WIN_COMBINATIONS.detect do |win_combo|
+    if (@board[win_combo[0]]) == "X" && (@board[win_combo[1]]) == "X" && (@board[win_combo[2]]) == "X"
+      return win_combo
+    elsif (@board[win_combo[0]]) == "O" && (@board[win_combo[1]]) == "O" && (@board[win_combo[2]]) == "O"
+      return win_combo
+    end
+      false
+  end
+end
  
   def turn_count
     @board.count{|token| token == "X" || token == "O"}
